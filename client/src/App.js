65a005b84caee6b3b46050e18248cd1a8495607f
Tatjana Svjatova <tsv.mailbox@gmail.com>
2020-12-14 11:31:33 +0200

    todos component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import Login from "./Components/Login";
 import Register from "./Components/Register";
+import Todos from "./Components/Todos";
 function App() {
   return (
     <Router>
@@ -11,6 +12,7 @@ function App() {
       <Route exact path="/" component={Home} />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
+      <Route path="/todos" component={Todos} />
     </Router>
   );
 }
