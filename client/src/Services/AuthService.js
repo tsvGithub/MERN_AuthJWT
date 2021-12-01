c7b32dcc9ab6a75754bb0d7b10e8cf017b28e5b3
Tatjana Svjatova <tsv.mailbox@gmail.com>
2021-12-01 17:44:20 +0200

    authService fetch

--- client/src/Services/AuthService.js
+++ client/src/Services/AuthService.js
@@ -1,14 +1,14 @@
-//contain fetch request through endpoints that
+//'AuthService' contains fetch requests through endpoints that
 //created in BE
-//AuthService will store fetch
+
 //1
 //Object contains a buch of convenience functions:
 export default {
   //----------------------------------
-  // 1a
-  //func login takes a user
+  // 1a LOGIN
+  //function 'login' takes a 'user'
   login: (user) => {
-    console.log(user);
+    // console.log(user);
     //configuration for fetch:
     return fetch(
       //endpoint
@@ -17,10 +17,9 @@ export default {
       {
         //post request
         method: "post",
-        //body need send as JSON => convert to JSON
-        //pass in a user
+        //'body' need send as JSON => convert to JSON a 'user'
         body: JSON.stringify(user),
-        //headers let BE to know what is sending
+        //'headers' let BE to know what is sending
         headers: {
           "Content-Type": "application/json",
         },
@@ -29,20 +28,20 @@ export default {
     ).then((res) => {
       //
       if (res.status !== 401) {
-        //get data that parsed now
+        //get data that parsed
         return res.json().then((data) => data);
       }
       //if not authorized
       else {
-        //return false & empty strings for user
+        //return false & empty strings for 'user'
         return { isAuthenticated: false, user: { username: "", role: "" } };
       }
     });
   },
   //------------------------------------
-  //1b
+  //1b 'user'===username, password, role
   register: (user) => {
-    console.log(user);
+    // console.log(user);
     return fetch("/user/register", {
       method: "post",
       body: JSON.stringify(user),
@@ -57,21 +56,24 @@ export default {
   //1c
   logout: () => {
     return fetch("/user/logout")
+    //get response back and parse it
       .then((res) => res.json())
+      //parsed date
       .then((data) => data);
   },
   //------------------------------------------
   //1d
   //to persist authentication === сохранить аутентификацию
-  //once user is logeed in we set a state within
+  //once user is logeed in we set a 'state' within
   //REACT Component to let App know that user has
   //been authenticated.
-  //When you closte React app that state is gone.
-  //So isAuthenticated syncs BE & FE together
+  //When you close React app that state is gone.
+  //So isAuthenticated syncs BE & FE together and
+  //when visit site again => to stay logged in
   isAuthenticated: () => {
     return (
       fetch("/user/authenticated")
-        //
+        //get response
         .then((res) => {
           // if authentication fails, Passport will respond with a 401 Unauthorized status,
           //and any additional route handlers will not be invoked.
